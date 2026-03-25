a116ad23831debd916fb42fd74eb68968d9f4cf2
VigneshBaradhanQa <baradhanvignesh@gmail.com>
2026-03-25 10:24:06 +0530

    updated basetest and testng for browser parameter

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,6 +2,7 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.*;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -20,16 +21,20 @@ public class BaseTest {
     @Parameters("browser")
     public void setup(@Optional("chrome") String browser) {
 
-        if (browser.equalsIgnoreCase("chrome")) {
-            WebDriverManager.chromedriver().setup();
-            driver.set(new ChromeDriver());
-        } else {
-            throw new IllegalArgumentException("Browser not supported: " + browser);
-        }
+    	if (browser.equalsIgnoreCase("chrome")) {
+    	    WebDriverManager.chromedriver().setup();
+    	    driver.set(new ChromeDriver());
+    	} 
+    	else if (browser.equalsIgnoreCase("firefox")) {
+    	    WebDriverManager.firefoxdriver().setup();
+    	    driver.set(new FirefoxDriver());
+    	} 
+    	else {
+    	    throw new IllegalArgumentException("Browser not supported: " + browser);
+    	}
 
         getDriver().manage().window().maximize();
 
-        // Better to avoid implicit wait when using explicit waits heavily
         getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
 
         getDriver().get(ConfigReader.get("url"));
